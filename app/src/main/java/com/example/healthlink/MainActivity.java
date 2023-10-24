998a6ae249691714830e97e07bf2733503028b59
IsmailAlamir <ismaeelalamir@gmail.com>
2023-10-24 21:53:17 +0300

    ADD : Doctor cards / RV

--- app/src/main/java/com/example/healthlink/MainActivity.java
+++ app/src/main/java/com/example/healthlink/MainActivity.java
@@ -1,15 +1,17 @@
 package com.example.healthlink;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
 
-import com.example.healthlink.R;
-import com.example.healthlink.profileSetting.User;
-import com.google.gson.Gson;
+import com.example.healthlink.Doctors.Doctor;
+import com.example.healthlink.Doctors.DoctorAdapter;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
@@ -18,26 +20,39 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    DoctorAdapter doctorAdapter;
+    RecyclerView doctorRecyclerView;
+    List<Doctor> doctors=new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initRecycleView();
         new FetchData().start();
+
     }
 
+    private void initRecycleView() {
+
+        doctorRecyclerView=findViewById(R.id.rv_doctors);
+        doctorRecyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
+        doctorAdapter=new DoctorAdapter(doctors);
+        doctorRecyclerView.setAdapter(doctorAdapter);
+
+    }
 
 
     class FetchData extends Thread{
 
         String data= "";
         public void run() {
-
-
-
                 try {
                     URL url =new URL("https://dummyjson.com/users");
                     HttpURLConnection connection = (HttpURLConnection)url.openConnection();
@@ -61,38 +76,39 @@ public class MainActivity extends AppCompatActivity {
                         Log.i("API Parsing", String.valueOf(users.length())); //30
 
 
-//                        onBoardingItems.add(new OnBoardingItem("title","disc",img));
+                        for (int i = 0 ; i< users.length();i++){
 
-//                        for (int i = 0 ; i< users.length();i++){
-//                            JSONObject names= users.getJSONObject(i);
-//                            String name =names.getString("firstName");
-//                            userList.add(name);
-//
-//                        }
-                    }
+                            JSONObject doctor= users.getJSONObject(i);
 
-//                    line = line.replace("quoteText", "text");
-//                    line = line.replace("quoteAuthor", "author");
+                            String fullName =doctor.getString("firstName")+" "+doctor.getString("lastName");
+
+                            JSONObject address = doctor.getJSONObject("address");
+                            String city = address.getString("city") + " , " + address.getString("address");
+                            String phone = doctor.getString("phone");
+                            String imgSrc = doctor.getString("image");
 
 
-                    Gson gson = new Gson();
-                    User quotesAndAuthor = gson.fromJson(line, User.class);
+                            final Doctor newDoctor = new Doctor(fullName, city, phone, imgSrc);
+
+                            // Add the doctor to the list on the main thread
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    doctors.add(newDoctor);
+                                    doctorAdapter.notifyDataSetChanged(); // Notify the adapter of data changes
+                                }
+                            });
+                        }
+                        }
 
 
-//                while (line!=null){
-//                    System.out.println(line);
-//                    line=in.readLine();
-//                    in.close();
-//                }
-
-
-                }catch (Exception e){
-                    Log.e("API Parsing", "error");
+                }catch (JSONException | IOException e){
+                    Log.e("API Parsing", String.valueOf(e));
 
                 }
 
 
-            }
+        }
     }
 }
 
